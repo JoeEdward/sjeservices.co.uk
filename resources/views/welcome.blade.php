d96a180bcf02cd048f2bd84da3ae66ab6a313415
Joe Edwards <joe@Joes-MacBook-Pro.local>
2017-02-10 15:12:15 +0000

    chanfe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SjeServices
                 </div>
 
                 <div class="links">
